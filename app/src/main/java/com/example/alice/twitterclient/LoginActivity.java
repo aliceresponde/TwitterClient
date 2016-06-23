9c38b719ab4876c633c7e670016d721cc8328af9
Alicia Beltran <aliceresponde@gmail.com>
2016-06-22 21:15:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alice/twitterclient/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.alice.twitterclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "aliceresponde@gmail.com";
+    private static final String TWITTER_SECRET = "200415651ua";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig), new Crashlytics());
+        setContentView(R.layout.activity_login);
+    }
+}
